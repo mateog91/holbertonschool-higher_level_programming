7684bb98c4ed0e1d04c07817e921d3837cbe27b4
mateog91 <mateog91@gmail.com>
2021-09-08 21:27:03 +0000

    FIX: hidden_4 inside dir

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for element in dir():
-        if element[0] != '_':
-            if element[1] != '_':
-                print(element)
+    for element in dir(hidden_4):
+        if not element.startswith("__"):
+            print(element)
