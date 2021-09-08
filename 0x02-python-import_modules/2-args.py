0caceb1c71fedb689b7cc732167a16c347cb610f
mateog91 <mateog91@gmail.com>
2021-09-08 18:57:48 +0000

    FIX: not print first arg, count corrected

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    length = len(sys.argv)
+    length = len(sys.argv) - 1
     print("{} arguments:".format(length))
-    for i in range(length):
+    for i in range(1, length + 1):
         print("{}: {}".format(i, sys.argv[i]))
