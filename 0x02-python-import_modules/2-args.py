cf8156d43bc598d34011cb426d674f42ac37e7f8
mateog91 <mateog91@gmail.com>
2021-09-08 19:12:34 +0000

    FIX: corrected text when only one argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,11 @@
 if __name__ == "__main__":
     import sys
     length = len(sys.argv) - 1
-    print("{} arguments:".format(length))
+    if length == 0:
+        txt = "."
+    else:
+        txt = ":"
+
+    print("{} arguments{}".format(length, txt))
     for i in range(1, length + 1):
         print("{}: {}".format(i, sys.argv[i]))
