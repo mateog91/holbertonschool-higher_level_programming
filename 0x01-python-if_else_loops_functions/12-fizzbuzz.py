3383ecaa0eff1818d5208828d1ac668a403f1661
mateog91 <mateog91@gmail.com>
2021-09-08 01:53:22 +0000

    STYLE: corrected extra space

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for number in range (1, 101):
+    for number in range(1, 101):
         if number % 3 == 0:
             if number % 5 == 0:
                 print_value = "FizzBuzz"
