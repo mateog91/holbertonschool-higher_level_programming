58ad7c7e15c6d5cb329ad489a515d258ee76d010
mateog91 <mateog91@gmail.com>
2021-09-08 22:02:03 +0000

    intento de christian

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     for element in dir(hidden_4):
-        if not element.startswith("__"):
+        if element[:2] != "__":
             print(element)
