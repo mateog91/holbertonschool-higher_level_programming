02900c669028da736a5261d5962675a76b54d8d5
mateog91 <mateog91@gmail.com>
2021-09-13 21:59:16 +0000

    FIX: added space between numbers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,10 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return None
     for row in matrix:
+        first = True
         for element in row:
+            if first == False:
+                print(" ", end="")
             print("{:d}".format(element), end="")
+            first = False
         print()
