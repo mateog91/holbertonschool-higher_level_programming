1627e47695973fb3408664892b266b1e0e2247a4
Mateo <mateog91@gmail.com>
2021-09-15 17:16:36 -0500

    FIX: remvoved teste cases

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,9 +2,3 @@
 def common_elements(set_1, set_2):
 
     return [e1 for e1 in set_1 for e2 in set_2 if e1 == e2]
-
-
-set_1 = {"Python", "C", "Javascript"}
-set_2 = {"Bash", "C", "Ruby", "Perl"}
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
