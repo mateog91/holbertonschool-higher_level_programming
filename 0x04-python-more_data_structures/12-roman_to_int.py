be16b245f7be14574ff750b442fa85049280b2cc
Mateo <mateog91@gmail.com>
2021-09-15 23:59:39 -0500

    ADD

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    num_directory = {'I': 1, 'V': 5, 'X': 10,
+                     'L': 50, 'C': 100, 'D': 500, 'M': 100}
+
+    num_list = []
+    for key in roman_string:
+        num_list.append(num_directory[key])
+
+    num_list.reverse()
+    i = 0
+    sum = num_list[0]
+    while(i < len(num_list) - 1):
+        if(num_list[i] > num_list[i + 1]):
+            sum = sum - num_list[i+1]
+        else:
+            sum = sum + num_list[i+1]
+    return sum
