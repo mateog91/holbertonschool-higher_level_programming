7f302be8fda1bbbf61c515cde8dd286afca430b1
Mateo <mateog91@gmail.com>
2021-09-15 19:59:40 -0500

    FIX: reversed changes and add empty dictionary validation

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
-    keys_list = list(a_dictionary.keys())
-    best_value = a_dictionary[keys_list[0]]
+    best_value = 0
     for key in a_dictionary:
         current_value = a_dictionary[key]
         if current_value > best_value:
