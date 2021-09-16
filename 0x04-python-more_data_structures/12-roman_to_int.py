d1525c18e4aa876424725f91c1f2822fba5965fd
Mateo <mateog91@gmail.com>
2021-09-16 00:11:55 -0500

    FIX: return

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or roman_string == "" or roman_string is not str:
+    if roman_string is None or roman_string == "" or not roman_string is str:
+        return 0
 
     num_directory = {'I': 1, 'V': 5, 'X': 10,
                      'L': 50, 'C': 100, 'D': 500, 'M': 100}
