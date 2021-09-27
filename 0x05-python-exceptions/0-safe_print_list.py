3611012f7f205ff96c1452a207cdefaa6f7ee0fe
Mateo <mateog91@gmail.com>
2021-09-27 15:49:50 -0500

    FIX: always declare i

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range(0, x):
+        i = 0
+        while i < x:
             print(my_list[i], end="")
+            i += 1
 
     except IndexError:
         print()
