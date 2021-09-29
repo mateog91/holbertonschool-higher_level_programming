a46bc56e41e2892cc3d53b72650cdd22a83ae981
Mateo <mateog91@gmail.com>
2021-09-28 22:44:40 -0500

    ADD: FIle 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' Definitionb of Class'''
+
+
+class Square:
+    '''Class Square'''
+
+    def __init__(self, size):
+        '''
+        Initializing  size of square
+
+        Attributes:
+            size (int): Square's size
+        '''
+        self.__size = size
