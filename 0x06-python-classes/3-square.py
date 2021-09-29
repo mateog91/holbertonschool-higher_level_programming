15ccd66a6b5eecdeb7216dba044c41b72faeeecd
Mateo <mateog91@gmail.com>
2021-09-28 23:30:30 -0500

    UPD: UPLOAD AGAIN

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -20,21 +20,6 @@ class Square:
         else:
             self.__size = size
 
-    @property  # getter
-    def size(self):
-        '''getter of size'''
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        '''size setter'''
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
     def area(self):
-        '''Calculates area of square'''
+        '''calculate area of square'''
         return self.__size * self.__size
