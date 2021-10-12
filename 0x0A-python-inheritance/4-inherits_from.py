ac89f6253d1c2954c50f4c5e0145ffde24b19fb2
Mateo <mateog91@gmail.com>
2021-10-11 22:54:57 -0500

    redo

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,8 +6,7 @@
 
 def inherits_from(obj, a_class):
     """returns ture if is instance of a class inhereted otherwise False"""
-    if issubclass(type(obj), a_class):
-        if type(obj) is a_class:
-            return False
-        else:
-            return True
+    if type(obj) is not a_class:
+        return issubclass(type(obj), a_class)
+    else:
+        return False
