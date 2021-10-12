99da6bf4ea3e70f5ecbf0ba2ea877fa69c13471f
Mateo <mateog91@gmail.com>
2021-10-12 17:51:42 -0500

    ADD: FILE 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for Task 5
+    Write a function that creates an Object from a “JSON file”:"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Write a function that creates an Object from a “JSON file”:"""
+    with open(filename, mode="r", encoding="UTF-8") as myFile:
+        json.load(myFile)
