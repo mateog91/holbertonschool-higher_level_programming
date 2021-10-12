32c515196963eb58d12f1df500ae8f72eb44da16
Mateo <mateog91@gmail.com>
2021-10-11 23:03:30 -0500

    ADD: FILE 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module with an empty Class"""
+
+
+class BaseGeometry:
+    """BaseGeometry Class"""
+
+    def area(self):
+        """finds area of the geometric figure"""
+        raise Exception("area() is not implemented")
