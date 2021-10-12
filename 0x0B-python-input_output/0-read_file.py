f4dc7776f2eff00ac37fd4a1231d91cbab98f231
Mateo <mateog91@gmail.com>
2021-10-12 16:59:42 -0500

    REMOVED TEST CASE

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,8 +6,3 @@ def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout:"""
     with open(filename, encoding="utf-8") as myFile:
         print(myFile.read())
-
-
-read_file = __import__('0-read_file').read_file
-
-read_file("my_file_0.txt")
