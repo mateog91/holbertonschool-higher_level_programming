46c33806787fd1a8d81e33f5ea8e63a240bb2651
Mateo <mateog91@gmail.com>
2021-10-11 22:38:50 -0500

    ADD: FILE 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Write a function that returns True if the object is an instance of a
+    class that inherited (directly or indirectly) from the specified class ;
+    otherwise False."""
+
+
+def inherits_from(obj, a_class):
+    """returns ture if is instance of a class inhereted otherwise False"""
+    if isinstance(obj, a_class):
+        if not type(obj) is a_class:
+            return True
