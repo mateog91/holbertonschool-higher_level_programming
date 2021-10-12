4e7d62a2fd344f181877f60900d096fbecbb6ab5
Mateo <mateog91@gmail.com>
2021-10-12 01:54:17 -0500

    ADD file 100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,7 +6,11 @@ class MyInt(int):
     """MyInt is a rebel. MyInt has == and != operators inverted"""
 
     def __eq__(self, x: object):
+        """overrides the magic method into this new one by calling it into the
+            upper class int method ne"""
         return super().__ne__(x)
 
     def __ne__(self, x: object):
+        """overrides the magic method into this new one by calling it into the
+            upper class int method eq"""
         return super().__eq__(x)
