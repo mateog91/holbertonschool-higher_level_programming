7bf8cbd1e3766bf3df4eee8dfed618b54b10d6f8
Mateo <mateog91@gmail.com>
2021-10-12 16:20:31 -0500

    ADD: FILE 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,7 +8,3 @@ def write_file(filename="", text=""):
     """returns the number of characters written:"""
     with open(filename, mode="w", encoding="utf-8") as myFile:
         return myFile.write(text)
-
-
-nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
-print(nb_characters)
