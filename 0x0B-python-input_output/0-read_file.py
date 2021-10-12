268d05b5a851a1fff9a3c8056fac9f7450cc972e
Mateo <mateog91@gmail.com>
2021-10-12 17:20:04 -0500

    UPD: added end=''

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout:"""
-    with open(filename, mode+"r", encoding="UTF-8") as myFile:
-        print(myFile.read())
+    with open(filename, mode="r", encoding="UTF-8") as myFile:
+        print(myFile.read(), end="")
