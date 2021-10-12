33f324a881930c3b51f44f3d4b8f89c2d8a55edb
Mateo <mateog91@gmail.com>
2021-10-12 00:28:48 -0500

    ADD: FILE 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,34 +1,10 @@
 #!/usr/bin/python3
 """Geometry Module"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Rectangle(BaseGeometry):
-    """Rectangle Geometric figure"""
-
-    def __init__(self, width, height):
-        """
-        Initializing Rectange
-
-        Attritutes:
-            width (int): width of rectangle, must be int greater than 0
-            height (int): height of rectangle, must be int greater than 0
-        """
-        BaseGeometry.integer_validator(self, "width", width)
-        BaseGeometry.integer_validator(self, "height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """finds area of the geometric figure"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-
-class Square:
-    """subclass square"""
+class Square(Rectangle):
+    """subclass of """
 
     def __init__(self, size):
         """initialization size of square
@@ -37,7 +13,8 @@ class Square:
             size (int): Square's size, must be and integer greater or equal
             than cero
         """
-        BaseGeometry.integer_validator(self, "size", size)
+
+        Rectangle.integer_validator(self, "size", size)
         self.__size = size
 
     def area(self, size):
