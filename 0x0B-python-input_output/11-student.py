5d082a3cd2294d14bbcbf5416d6fc5fd25fba20d
Mateo <mateog91@gmail.com>
2021-10-12 23:57:21 -0500

    ADD: file 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module that creates a Class Student"""
+
+
+class Student:
+    """class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialiation of instances of Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """that retrieves a dictionary representation
+            of a Student instance """
+        if isinstance(attrs, list) and all(isinstance(e, list) for e in attrs):
+            return {e: getattr(self, e) for e in attrs if hasattr(self, e)}
+        else:
+            return vars(self)
+
+    def reload_from_json(self, json):
+        """ replaces all attributes of the Student instance """
+        for key, value in json.items():
+            setattr(self, key, value)
