4d19605b6c5620de3659ea198c9a306d52c6c86a
Mateo <mateog91@gmail.com>
2021-10-13 11:17:42 -0500

    FIX: removed test case

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -14,19 +14,3 @@ def add_attribute(object, key, value):
         object.key = value
     else:
         raise TypeError("can't add new attribute")
-
-
-class MyClass():
-    pass
-
-
-mc = MyClass()
-add_attribute(mc, "name", "John")
-print(mc.name)
-
-try:
-    a = "My String"
-    add_attribute(a, "name", "Bob")
-    print(a.name)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
