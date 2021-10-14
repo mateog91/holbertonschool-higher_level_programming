1250a30e647dc7a4c7b405fdbba7bb7cb822e0a9
Mateo <mateog91@gmail.com>
2021-10-13 23:57:16 -0500

    r

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -18,3 +18,23 @@ class Student:
             return {e: getattr(self, e) for e in attrs if hasattr(self, e)}
         else:
             return vars(self)
+            hasattr()
+
+
+def print_dict(d, t=0):
+    keys = list(d.keys())
+    keys.sort()
+    for k in keys:
+        v = d[k]
+        if type(v) is dict:
+            t += 1
+            print_dict(v, t)
+        else:
+            print("{}{} => {} / {}".format("\t" * t, k, v, type(v)))
+
+
+student = Student("Tom", "Smith", 89)
+
+j_student = student.to_json(['first_name'])
+print(type(j_student))
+print_dict(j_student)
