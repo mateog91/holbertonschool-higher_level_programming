fc07b798b4ea44258049a6451b276701abd8ffe5
mateog91 <mateog91@gmail.com>
2021-11-17 23:31:04 +0000

    FIX: burned database name

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -5,6 +5,6 @@
 --    The database name will be passed as an argument of the mysql command
 --    If the table id_not_null already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+        id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(256) NOT NULL);
