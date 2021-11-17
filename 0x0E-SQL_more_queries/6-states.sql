434e269e9ef621e61bb5eaed89a0422dc48b5306
mateog91 <mateog91@gmail.com>
2021-11-17 23:15:42 +0000

    ADD: FILE 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- Write a script that creates the table id_not_null on your MySQL server.
+--    id_not_null description:
+--        id INT with the default value 1
+--        name VARCHAR(256)
+--    The database name will be passed as an argument of the mysql command
+--    If the table id_not_null already exists, your script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(256) NOT NULL);
