99027d64032dd6217e990086a77e3a6bb30c5c5c
mateog91 <mateog91@gmail.com>
2021-11-18 00:19:52 +0000

    ADD FILE 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Write a script that creates the table id_not_null on your MySQL server.
+--    id_not_null description:
+--        id INT with the default value 1
+--        name VARCHAR(256)
+--    The database name will be passed as an argument of the mysql command
+--    If the table id_not_null already exists, your script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(id),
+        name VARCHAR(256) NOT NULL);
