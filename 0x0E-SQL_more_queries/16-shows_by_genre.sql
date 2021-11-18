c831f79f34f23c498b2bfbd5dcd4b9e0ccd17737
mateog91 <mateog91@gmail.com>
2021-11-18 22:58:34 +0000

    FIX

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
+SELECT tv_shows.title, tv_genres.name
+FROM tv_show_genres
+RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name;
