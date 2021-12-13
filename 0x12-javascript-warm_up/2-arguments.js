31fb22b742587120d32850b82df7c72e29e4297b
Mateo <mateog91@gmail.com>
2021-12-13 16:40:52 -0500

    ADD: file 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-if (argv.length > 2) {
-  console.log('Argument found');
+if (argv.length > 3) {
+  console.log('Arguments found');
+} else if (argv.length > 2) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
