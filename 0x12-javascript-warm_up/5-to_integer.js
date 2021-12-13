7f46ab5d3aca5726a614b0d1482488d18d359350
Mateo <mateog91@gmail.com>
2021-12-13 18:21:54 -0500

    ADD: FILE 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-const argv1 = argv[2];
+const argv1 = parseInt(argv[2]);
 if (isNaN(argv1)) {
   console.log('Not a number');
 } else {
-  console.log(parseInt(argv1));
+  console.log((argv1));
 }
