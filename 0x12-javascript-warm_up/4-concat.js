62ea4de7e91ad5841b3b88bd2a56f23202faf411
Mateo <mateog91@gmail.com>
2021-12-13 17:55:59 -0500

    fix style

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,11 +4,11 @@ const { argv } = require('process');
 let argv1 = 'undefined';
 let argv2 = 'undefined';
 argv.forEach((val, index) => {
-    if (index === 2) {
-        argv1 = val;
-    }
-    if (index === 3) {
-        argv2 = val;
-    }
+  if (index === 2) {
+    argv1 = val;
+  }
+  if (index === 3) {
+    argv2 = val;
+  }
 });
 console.log(`${argv1} is ${argv2}`);
