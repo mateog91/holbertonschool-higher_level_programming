c6b9049a0fcfc4b0b9269f2e63528e0241634e8f
Mateo <mateog91@gmail.com>
2021-12-13 19:36:18 -0500

    optimized with repeat

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,15 +2,12 @@
 const { argv } = require('process');
 
 const argv1 = parseInt(argv[2]);
+const str = 'X';
 
 if (isNaN(argv1)) {
   console.log('Missing size');
 } else if (argv1 > 0) {
   for (let i = 0; i < argv1; i++) {
-    const arr = [];
-    for (let j = 0; j < argv1; j++) {
-      arr.push('X');
-    }
-    console.log(arr.join(''));
+    console.log(str.repeat(argv1));
   }
 }
