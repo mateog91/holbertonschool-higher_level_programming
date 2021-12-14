c6ae2c77acf9b9ee13251f4f72e3632aeabfa5d1
Mateo <mateog91@gmail.com>
2021-12-14 16:46:01 -0500

    ADD: FILE 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+/** Define Rectangle class */
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const str = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(str.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
