2d638c3f719503b5a55b6f04dc590934a3504288
Mateo <mateog91@gmail.com>
2021-12-13 19:59:31 -0500

    FIX

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-const { argv } = require('process');
 
-function add(a, b) {
+function add (a, b) {
   return (a + b);
 }
 
+const { argv } = require('process');
 const a = parseInt(argv[2]);
 const b = parseInt(argv[3]);
-if (b) {
-  console.log(add(a, b));
-} else {
-  console.log(b);
-}
+
+console.log(add(a, b));
