55498ec125f0f76f16d1f88bb8f21ad55a6125a4
Mateo <mateog91@gmail.com>
2021-12-13 19:21:41 -0500

    ADD: file 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const { argv } = require('process');
+
+const argv1 = parseInt(argv[2]);
+
+if (isNaN(argv1)) {
+  console.log('Missing number of occurrences');
+} else if (argv1 > 0) {
+  for (let i = 0; i < argv1; i++) {
+    let arr = []
+    for (let j = 0; j < argv1; j++) {
+      arr.push('X')
+    }
+    console.log(arr.join(''));
+  }
+}
