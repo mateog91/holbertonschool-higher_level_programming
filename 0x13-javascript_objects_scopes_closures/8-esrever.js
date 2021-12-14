cd5e0333cb4f97b80a5904fb44f4ea7b8f6837ae
Mateo <mateog91@gmail.com>
2021-12-14 18:46:17 -0500

    ADD: FILE 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/** function that returns the reversed version of a list: */
+exports.esrever = function (list) {
+  const rlist = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    rlist.push(list[i]);
+  }
+  return rlist;
+};
