181a1e0cb981a9e7e7414f5f23b38b7d9918b922
Mateo <mateog91@gmail.com>
2021-12-18 15:48:26 -0500

    FIX: nullable False

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,20 +2,18 @@
 """contains the class definition of a City
     and an instance Base = declarative_base():
 """
-if __name__ == "__main__":
-    from sqlalchemy import create_engine
-    from sqlalchemy.ext.declarative import declarative_base
-    from sqlalchemy import Column, Integer, String
-    from sqlalchemy import ForeignKey
 
-    Base = declarative_base()
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+Base = declarative_base()
 
-    class City(Base):
-        """ Class for City inherited from Base
-        """
-        __tablename__ = 'cities'
 
-        id = Column(Integer, autoincrement=True,
-                    nullable=False, primary_key=True)
-        name = Column(String(128), nullable=False)
-        state_id = Column(Integer, ForeignKey('states.id'))
+class City(Base):
+    """ Class for City inherited from Base
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id', nullable=False))
