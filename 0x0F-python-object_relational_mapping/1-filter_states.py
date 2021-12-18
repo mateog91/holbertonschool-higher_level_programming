36568380509dc5d25b093976dbe6b00201459264
Mateo <mateog91@gmail.com>
2021-12-18 13:53:59 -0500

    FIX STYLE

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" lists all states with a name starting with N (upper N) from the database
+    hbtn_0e_0_usa:
+"""
 if __name__ == "__main__":
     import sys
     import MySQLdb
