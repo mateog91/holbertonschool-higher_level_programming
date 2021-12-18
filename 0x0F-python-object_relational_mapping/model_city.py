1569886f2011e70cfb849bb26f1244ba529f8d5f
Mateo <mateog91@gmail.com>
2021-12-18 14:37:05 -0500

    FIX: dont execute when import and pycode style

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
+"""contains the class definition of a State
+    and an instance Base = declarative_base():
+"""
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy import Column, Integer, String
+    from sqlalchemy import ForeignKey
 
-Base = declarative_base()
+    Base = declarative_base()
 
+    class City(Base):
+        __tablename__ = 'cities'
 
-class City(Base):
-    __tablename__ = 'cities'
-
-    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+        id = Column(Integer, autoincrement=True,
+                    nullable=False, primary_key=True)
+        name = Column(String(128), nullable=False)
+        state_id = Column(Integer, ForeignKey('states.id'))
