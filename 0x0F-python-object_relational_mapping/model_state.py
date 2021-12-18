9e7dec79eb179c763ca7e7d592e01df0336b0a47
Mateo <mateog91@gmail.com>
2021-12-18 14:52:47 -0500

    FIX: removed prevent executing when import

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,18 +2,14 @@
 """contains the class definition of a State
     and an instance Base = declarative_base():
 """
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+Base = declarative_base()
 
-if __name__ == "__main__":
-    from sqlalchemy import create_engine
-    from sqlalchemy.ext.declarative import declarative_base
-    from sqlalchemy import Column, Integer, String
 
-    Base = declarative_base()
-
-    class State(Base):
-        """ Class for State inherited from Base
-        """
-        __tablename__ = 'states'
-
-        id = Column(Integer, primary_key=True, autoincrement=True)
-        name = Column(String(128), nullable=False)
+class State(Base):
+    """ Class for State inherited from Base
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
