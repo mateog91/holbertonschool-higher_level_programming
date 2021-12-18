1a7c570c26ee2caaa96c038b9941c3182e17eb1c
Mateo <mateog91@gmail.com>
2021-12-18 15:39:15 -0500

    FIX: base.metadata before Session

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,8 +2,6 @@
 """lists all State objects that contain the letter a from the
 database hbtn_0e_6_usa
 """
-from sys import argv
-
 
 if __name__ == "__main__":
     import sys
@@ -15,9 +13,9 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    Base.metadata.create_all(engine)
 
     for s, c in session.query(State, City).\
             filter(State.id == City.state_id).\
