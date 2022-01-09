19bf70eab83a28256b24cefcfbeb270c678f9fc8
Mateo <mateog91@gmail.com>
2022-01-09 18:47:40 -0500

    FIX: url assignment with argv

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """Write a Python script that takes in a URL, sends a request to the URL
     and displays the value of the variable X-Request-Id in the response header
-
-    You must use the packages requests and sys
-    You are not allow to import other packages than requests and sys
-    The value of this variable is different for each request
-    You don’t need to check script arguments (number and type)
+    - You must use the packages requests and sys
+    - You are not allow to import other packages than requests and sys
+    - The value of this variable is different for each request
+    - You don’t need to check script arguments (number and type)
 """
 
 if __name__ == "__main__":
     import requests
+    import sys
 
-    url = 'https://intranet.hbtn.io/status'
+    url = sys.argv[1]
     response = requests.get(url)
     print(response.headers['X-Request-Id'])
