c3eaae5d832544352ce8d62f32b342870be862e7
Mateo <mateog91@gmail.com>
2022-01-09 18:53:18 -0500

    FIX: changed using get method

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-"""Write a Python script that takes in a URL, sends a request to the URL
-"""
+"""Write a Python script that takes in a URL, sends a request to the
+    URL and displays the value of the variable X-Request-Id in the
+    response header
+    You must use the packages requests and sys
+    You are not allow to import other packages than requests and sys
+    The value of this variable is different for each request
+    You don’t need to check script arguments (number and type)
+    """
 
 if __name__ == "__main__":
     import requests
@@ -8,4 +14,4 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
     response = requests.get(url)
-    print(response.headers['X-Request-Id'])
+    print(response.headers.get('X-Request-Id'))
