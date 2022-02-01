2adc518e4f47daefb6e88051fcc1b31241e73ad9
Mateo <mateog91@gmail.com>
2022-02-01 17:16:43 -0500

    ADD: file 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const fs = require("fs");
+const { argv } = require("process");
+const request = require("request");
+
+const url = argv[2];
+
+request(url, (error, res) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`code: ${res.statusCode}`);
+  }
+});
